1b5c05ee4eef3cb8a4be803747e91ef318f1c415
hiroki tamura <hiirookii0625@gmail.com>
2020-09-03 15:29:14 +0900

    propertiesコントローラーの修正

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,34 +1,27 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties
-  # GET /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
+    2.times { @property.nearest_stations.build }
   end
 
-  # GET /properties/1/edit
   def edit
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
 
     respond_to do |format|
       if @property.save
-        format.html { redirect_to @property, notice: 'Property was successfully created.' }
+        format.html { redirect_to @property, notice: '物件を登録しました' }
         format.json { render :show, status: :created, location: @property }
       else
         format.html { render :new }
@@ -37,12 +30,10 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
-        format.html { redirect_to @property, notice: 'Property was successfully updated.' }
+        format.html { redirect_to @property, notice: '物件を編集しました' }
         format.json { render :show, status: :ok, location: @property }
       else
         format.html { render :edit }
@@ -51,8 +42,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
@@ -62,13 +51,26 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def property_params
-      params.require(:property).permit(:name, :rent, :address, :age, :remarks)
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(
+      :name,
+      :rent,
+      :address,
+      :age,
+      :remarks,
+      nearest_stations_attributes:[
+        route:,
+        station:,
+        minutes_walk:,
+        :id,
+        :_destroy,
+      ]
+    )
+  end
+
 end
