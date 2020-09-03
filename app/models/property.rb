1b24284dc19795354c03c9787a8aac25138e0bfa
hiroki tamura <hiirookii0625@gmail.com>
2020-09-03 21:40:05 +0900

    バリデーションの設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,10 @@
 class Property < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :address
+  end
+  validates :rent, presence: true, numericality:  { greater_than_or_equal_to: 0 }
+  validates :age, presence: true, numericality:  { greater_than_or_equal_to: 0 }
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
 end
